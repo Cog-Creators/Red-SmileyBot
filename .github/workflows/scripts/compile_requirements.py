16119239aad795e0f689849b9ea5ca7ab201598a
Jakub Kuczys <me@jacken.men>
2024-12-24 04:55:15 +0100

    Revert "Attempt setting Require-Python constraints"
    This reverts commit c18f7a7754e3f35ffe98acac9637a1a7b87a9141.

--- .github/workflows/scripts/compile_requirements.py
+++ .github/workflows/scripts/compile_requirements.py
@@ -3,9 +3,7 @@ import re
 import shutil
 import subprocess
 import sys
-import tempfile
 from pathlib import Path
-from typing import Tuple
 
 
 EXCLUDE_STEM_RE = re.compile(r".*-3\.(?!8-)(\d+)-extra-(doc|style)")
@@ -14,32 +12,18 @@ REQUIREMENTS_FOLDER = Path(__file__).parents[3].absolute() / "requirements"
 os.chdir(REQUIREMENTS_FOLDER)
 
 
-def _get_global_executable(version: str) -> Tuple[str, ...]:
-    return ("py", f"-{version}") if sys.platform == "win32" else (f"python{version}",)
-
-
-def _get_venv_executable(venv_dir: str) -> str:
-    return (
-        f"{venv_dir}{os.sep}Scripts{os.sep}python.exe"
-        if sys.platform == "win32"
-        else f"{venv_dir}{os.sep}bin{os.sep}python"
-    )
-
-
 def pip_compile(version: str, name: str) -> None:
     stem = f"{sys.platform}-{version}-{name}"
     if EXCLUDE_STEM_RE.fullmatch(stem):
         return
 
-    executable = _get_global_executable(version)
+    executable = ("py", f"-{version}") if sys.platform == "win32" else (f"python{version}",)
     subprocess.check_call(
         (
             *executable,
             "-m",
             "piptools",
             "compile",
-            "--pip-args",
-            "--python-version=3.8.1",
             "--upgrade",
             "--resolver=backtracking",
             "--verbose",
@@ -50,25 +34,12 @@ def pip_compile(version: str, name: str) -> None:
     )
 
 
-def validate_requirements(version: str) -> None:
-    files_it = REQUIREMENTS_FOLDER.glob(f"{sys.platform}-{version}-*.txt")
-
-    executable = _get_global_executable(version)
-    with tempfile.TemporaryDirectory() as venv_dir:
-        subprocess.check_call((*executable, "-m", "venv", venv_dir))
-        venv_executable = _get_venv_executable(venv_dir)
-
-        subprocess.check_call((venv_executable, "-m", "pip", "install", "pip-tools"))
-        subprocess.check_call((venv_executable, "-m", "piptools", "sync", *map(str, files_it)))
-
-
 for minor in range(8, 11 + 1):
     version = f"3.{minor}"
     pip_compile(version, "base")
     shutil.copyfile(f"{sys.platform}-{version}-base.txt", "base.txt")
     for file in REQUIREMENTS_FOLDER.glob("extra-*.in"):
         pip_compile(version, file.stem)
-    validate_requirements(version)
 
 with open(GITHUB_OUTPUT, "a", encoding="utf-8") as fp:
     fp.write(f"sys_platform={sys.platform}\n")
